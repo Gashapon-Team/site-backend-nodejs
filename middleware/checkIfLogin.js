12efbcc15fb377401207e4c39654d3d7b1d6c8ed
EZPAY_OA\shang.huang <shang.huang@ezpay.com.tw>
2023-07-04 17:56:57 +0800

    fix: change dev mode file path

--- middleware/checkIfLogin.js
+++ middleware/checkIfLogin.js
@@ -1,6 +1,6 @@
 const checkIfLogin = function(req, res, next) {
   res.send('驗證登入')
-  req.next()
+  res.next()
 }
 
 module.exports = checkIfLogin;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,15 @@
-var checkIfLogin = require('../middleware/checkIfLogin')
+// var checkIfLogin = require('../middleware/checkIfLogin')
 
 var express = require('express');
 var router = express.Router();
 
+// const checkIfLogin = function(req, res, next) {
+//   res.send('驗證登入')
+//   res.next()
+// }
+
 /* GET users listing. */
-router.get('/', checkIfLogin,function(req, res, next) {
+router.get('/',function(req, res, next) {
   res.status(200).json({message:'驗證成功，可使用該頁面'});
 });
 
