6139da809f2f47b9b9ce7ca3b6561a40a85d0dfb
EZPAY_OA\shang.huang <shang.huang@ezpay.com.tw>
2023-07-05 11:33:12 +0800

    feature: use env

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,11 @@ var checkIfLogin = require('../middleware/checkIfLogin')
 
 var express = require('express');
 var router = express.Router();
+const generateJWT = (user, statuscode, res) => {
+  const token = jwt.sign({id: user.id}, process.env.JWT_SECREAT, {
+    expirseIn: process.env.JWT_EXPIRSEDAY
+  });
+};
 
 /* GET users listing. */
 router.get('/',checkIfLogin ,function(req, res, next) {
@@ -13,7 +18,11 @@ router.get('/login' ,function(req, res, next) {
   const {email, password} = req.query;
   const data = {
     email,
-    password
+    password,
+    env:{
+      secreat: process.env.JWT_SECREAT,
+      expirsday: process.env.JWT_EXPIRSEDAY
+    }
   }
   res.status(200).json({
     message:'登入成功',
